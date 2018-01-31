e4cee265dfb47d52246ab09b55f1f7e52e9f6754
Діана Денисюк <user@A26COMP11>
2018-01-31 12:01:20 +0200

    homework 2.0

--- Project1/Project1/Source2.cpp
+++ Project1/Project1/Source2.cpp
@@ -1,7 +1,40 @@
 #include<iostream>
 using namespace std;
+void tringle(int high = 12);
 
 void main()
 {
+	/*int high;
+	cout << "Enter height ";
+	cin >> high;
+	system("cls");*/
+	tringle();
+}
 
+void tringle(int high)
+{
+		for (int i = 0; i < high; i++)
+		{
+			if (i == 0)
+			{
+				cout << "*";
+			}
+			else if (i == high - 1)
+			{
+				for (int j = 0; j < high; j++)
+				{
+					cout << "*";
+				}
+			}
+			else
+			{
+				cout << "*";
+				for (int j = 0; j < i - 1; j++)
+				{
+					cout << " ";
+				}
+				cout << "*";
+			}
+				cout << endl;
+		}
 }
